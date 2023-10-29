35cfb8c4a272fc96b5d17c4275668b1bccac8067
SHUBAM CHAUDHARY <beshubam@gmail.com>
2023-10-29 15:18:31 +0530

    commit5

--- src/pages/AddContest.jsx
+++ src/pages/AddContest.jsx
@@ -7,47 +7,94 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
+//import { Timestamp } from "firebase-admin/firestore";
 import { getAuth } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import ContestImage from "../images/contest.png";
+import firebase from "firebase/compat/app";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
 export default function AddContest() {
   const navigate = useNavigate();
   const auth = getAuth();
   const [loading, setLoading] = useState(false);
+  // const [formData, setFormData] = useState({
+  //   name: "Starter 1",
+  //   startingTime: "23rd January 2024, Wednesday, 5pm",
+  //   duration: 90,
+  //   link: "",
+  // });
+  // const { name, startingTime, duration, link } = formData;
+
+  // function onChange(e) {
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [e.target.id]: e.target.value,
+  //   }));
+  // }
+
   const [formData, setFormData] = useState({
     name: "Starter 1",
-    startingTime: "23rd January 2024, Wednesday, 5pm",
+    startingTime: null, // Initialize startingTime as null
     duration: 90,
     link: "",
   });
-  const { name, startingTime, duration, link } = formData;
 
-  function onChange(e) {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  }
+  const handleDateTimeChange = (newDateTime) => {
+    setFormData({ ...formData, startingTime: newDateTime });
+  };
 
-  async function onSubmit(e) {
-    e.preventDefault();
-    setLoading(true);
-    const formDataCopy = {
-      ...formData,
-      timestamp: serverTimestamp(),
-      //this is uid of the user
-      userRef: auth.currentUser.uid,
-    };
-    console.log(formDataCopy);
-    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
-    setLoading(false);
-    toast.success("contest added");
-    navigate(`/`);
-  }
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Convert the startingTime to a Firestore timestamp
+      const timestamp = firebase.firestore.Timestamp.fromDate(
+        formData.startingTime.toDate()
+      );
+      console.log(timestamp);
+      // Create a new Firestore document with the formData
+      await db.collection("listings").add({
+        name: formData.name,
+        startingTime: timestamp, // Store startingTime as a timestamp
+        duration: formData.duration,
+        link: formData.link,
+      });
+
+      // console.log("Form Data Submitted Successfully");
+      toast.success("contest added");
+      navigate(`/`);
+    } catch (error) {
+      console.error("Error submitting form data:", error);
+    }
+  };
+
+  // async function onSubmit(e) {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //   const formDataCopy = {
+  //     ...formData,
+  //     timestamp: serverTimestamp(),
+  //     //this is uid of the user
+  //     userRef: auth.currentUser.uid,
+  //   };
+  //   console.log(formDataCopy);
+  //   const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+  //   setLoading(false);
+  //   toast.success("contest added");
+  //   navigate(`/`);
+  // }
 
   if (loading) {
     return (
@@ -65,57 +112,106 @@ export default function AddContest() {
   }
 
   return (
-    <main className="max-w-[800px] px-4 mx-auto mt-[50px]">
-      <h1 className="text-3xl text-center mt-6 cursive">Add New Contest</h1>
-      <form onSubmit={onSubmit}>
-        <p className="text-lg mt-6 font-semibold">Contest Name</p>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={onChange}
-          placeholder="Contest Name"
-          required
-          className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  text-center"
-        />
+    <form onSubmit={handleSubmit}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DateTimePicker"]}>
+          <DateTimePicker
+            label="Starting Time"
+            value={formData.startingTime}
+            onChange={handleDateTimeChange}
+          />
+        </DemoContainer>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="duration">Duration (minutes):</label>
+          <input
+            type="number"
+            id="duration"
+            name="duration"
+            value={formData.duration}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="link">Link:</label>
+          <input
+            type="text"
+            id="link"
+            name="link"
+            value={formData.link}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </LocalizationProvider>
+    </form>
+    // <main className="max-w-[800px] px-4 mx-auto mt-[50px]">
+    //   <h1 className="text-3xl text-center mt-6 cursive">Add New Contest</h1>
+    //   <form onSubmit={onSubmit}>
+    //     <p className="text-lg mt-6 font-semibold">Contest Name</p>
+    //     <input
+    //       type="text"
+    //       id="name"
+    //       value={name}
+    //       onChange={onChange}
+    //       placeholder="Contest Name"
+    //       required
+    //       className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  text-center"
+    //     />
+    //     <div className="mb-[20px]">
+    //       <p className="text-lg font-semibold">Starting time</p>
+    //       <LocalizationProvider dateAdapter={AdapterDayjs}>
+    //         <DemoContainer components={["DateTimePicker"]}>
+    //           <DateTimePicker
+    //             label="Pick Time for Contest"
+    //             type="text"
+    //             id="startingTime"
+    //             value={startingTime}
+    //             onChange={onChange}
+    //             placeholder="startingTime"
+    //             required
+    //             className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  text-center"
+    //           />
+    //         </DemoContainer>
+    //       </LocalizationProvider>
+    //     </div>
 
-        <p className="text-lg font-semibold">Starting time</p>
-        <input
-          type="text"
-          id="startingTime"
-          value={startingTime}
-          onChange={onChange}
-          placeholder="23rd January 2024, Wednesday, 5pm"
-          required
-          className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  text-center"
-        />
-        <p className="text-lg font-semibold">Duration in minutes</p>
-        <input
-          type="number"
-          id="duration"
-          value={duration}
-          onChange={onChange}
-          required
-          className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300  text-center"
-        />
-        <p className="text-lg font-semibold">Contest Link</p>
-        <input
-          type="text"
-          id="link"
-          value={link}
-          onChange={onChange}
-          placeholder="Contest Link"
-          required
-          className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  mb-6 text-center"
-        />
-        <button
-          type="submit"
-          className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm  rounded shadow-md "
-        >
-          Add Contest
-        </button>
-      </form>
-    </main>
+    //     <p className="text-lg font-semibold">Duration in minutes</p>
+    //     <input
+    //       type="number"
+    //       id="duration"
+    //       value={duration}
+    //       onChange={onChange}
+    //       required
+    //       className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300  text-center"
+    //     />
+    //     <p className="text-lg font-semibold">Contest Link</p>
+    //     <input
+    //       type="text"
+    //       id="link"
+    //       value={link}
+    //       onChange={onChange}
+    //       placeholder="Contest Link"
+    //       required
+    //       className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded  mb-6 text-center"
+    //     />
+    //     <button
+    //       type="submit"
+    //       className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm  rounded shadow-md "
+    //     >
+    //       Add Contest
+    //     </button>
+    //   </form>
+    // </main>
   );
 }
 // import { useState } from "react";
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,112 +1,277 @@
+//
+
 import React, { useState, useEffect } from "react";
-import { Leaderboard } from "flywheel-leaderboard";
-import { SHA256, enc } from "crypto-js";
+import { classNames } from "primereact/utils";
+import { FilterMatchMode, FilterOperator } from "primereact/api";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import { MultiSelect } from "primereact/multiselect";
+import { Tag } from "primereact/tag";
+import { TriStateCheckbox } from "primereact/tristatecheckbox";
+import { CustomerService } from "./CustomerService";
 
-async function sha512(str) {
-  return crypto.subtle
-    .digest("SHA-512", new TextEncoder("utf-8").encode(str))
-    .then((buf) => {
-      return Array.prototype.map
-        .call(new Uint8Array(buf), (x) => ("00" + x.toString(16)).slice(-2))
-        .join("");
-    });
-}
+export default function Leaderboard() {
+  const [customers, setCustomers] = useState(null);
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    "country.name": { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    representative: { value: null, matchMode: FilterMatchMode.IN },
+    status: { value: null, matchMode: FilterMatchMode.EQUALS },
+    verified: { value: null, matchMode: FilterMatchMode.EQUALS },
+  });
+  const [loading, setLoading] = useState(true);
+  const [globalFilterValue, setGlobalFilterValue] = useState("");
+  const [representatives] = useState([
+    { name: "Amy Elsner", image: "amyelsner.png" },
+    { name: "Anna Fali", image: "annafali.png" },
+    { name: "Asiya Javayant", image: "asiyajavayant.png" },
+    { name: "Bernardo Dominic", image: "bernardodominic.png" },
+    { name: "Elwin Sharvill", image: "elwinsharvill.png" },
+    { name: "Ioni Bowcher", image: "ionibowcher.png" },
+    { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
+    { name: "Onyama Limba", image: "onyamalimba.png" },
+    { name: "Stephen Shaw", image: "stephenshaw.png" },
+    { name: "XuXue Feng", image: "xuxuefeng.png" },
+  ]);
+  const [statuses] = useState([
+    "unqualified",
+    "qualified",
+    "new",
+    "negotiation",
+    "renewal",
+  ]);
 
-export default function LeaderboardList() {
-  const [scoreArray, setScoreArray] = useState([]);
-  const contests = ["480776", "477676"]; // Add more contest IDs as needed
+  const getSeverity = (status) => {
+    switch (status) {
+      case "unqualified":
+        return "danger";
+
+      case "qualified":
+        return "success";
+
+      case "new":
+        return "info";
+
+      case "negotiation":
+        return "warning";
+
+      case "renewal":
+        return null;
+    }
+  };
 
   useEffect(() => {
-    async function fetchData() {
-      let updatedScoreArray = [];
+    CustomerService.getCustomersMedium().then((data) => {
+      setCustomers(getCustomers(data));
+      setLoading(false);
+    });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-      for (const contest_id of contests) {
-        const data = await getStandings(contest_id);
-        updatedScoreArray = getScores(data, contest_id, updatedScoreArray);
-      }
-      setScoreArray(updatedScoreArray);
-    }
+  const getCustomers = (data) => {
+    return [...(data || [])].map((d) => {
+      d.date = new Date(d.date);
 
-    fetchData();
-  }, []);
+      return d;
+    });
+  };
 
-  async function getStandings(contest_id) {
-    const rand = String(Math.floor(Math.random() * 100000)).padStart(6, "0");
-    const current_time = String(Math.floor(Date.now() / 1000));
-    const api_key = "7bcb2c2a57feab460343d341d164e26b4ae32fd1";
-    const api_secret = "3e67186659f5f9f35c5841127c40a5b0339180b1";
-    const api_sig =
-      rand +
-      "/contest.standings?apiKey=" +
-      api_key +
-      "&contestId=" +
-      contest_id +
-      "&time=" +
-      current_time +
-      "#" +
-      api_secret;
-    const hash = await sha512(api_sig);
-    const hashWithRand = rand + hash;
-    const url = `https://codeforces.com/api/contest.standings?contestId=${contest_id}&apiKey=${api_key}&time=${current_time}&apiSig=${hashWithRand}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  }
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
 
-  function getScores(obj, contestId, previousScores) {
-    const updatedScoreArray = [...previousScores];
-    let totalScore = 0;
+    _filters["global"].value = value;
 
-    for (const item of obj.result.problems) {
-      totalScore += item.rating;
-    }
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
 
-    for (const row of obj.result.rows) {
-      const points = row.points;
-      const penalty = row.penalty;
-      const handle = row.party.members[0].handle;
-      const rollno = 0;
-      let Score = 100 * points + (1 - penalty / totalScore);
-      Score = Score.toFixed(2);
+  const renderHeader = () => {
+    return (
+      <div className="flex justify-content-end">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={globalFilterValue}
+            onChange={onGlobalFilterChange}
+            placeholder="Keyword Search"
+          />
+        </span>
+      </div>
+    );
+  };
 
-      // Check if handle already exists in updatedScoreArray
-      const existingUserIndex = updatedScoreArray.findIndex(
-        (user) => user.handle === handle
-      );
+  const countryBodyTemplate = (rowData) => {
+    return (
+      <div className="flex align-items-center gap-2">
+        <img
+          alt="flag"
+          src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
+          className={`flag flag-${rowData.country.code}`}
+          style={{ width: "24px" }}
+        />
+        <span>{rowData.country.name}</span>
+      </div>
+    );
+  };
 
-      if (existingUserIndex !== -1) {
-        // Update the existing user's score for the contest
-        updatedScoreArray[existingUserIndex].Score = (
-          parseFloat(updatedScoreArray[existingUserIndex].Score) +
-          parseFloat(Score)
-        ).toFixed(2);
-      } else {
-        // Create a new entry for the user for the given contest
-        updatedScoreArray.push({
-          Score,
-          contestId,
-          handle,
-          rollno,
-        });
-      }
-    }
-    console.log(updatedScoreArray);
+  const representativeBodyTemplate = (rowData) => {
+    const representative = rowData.representative;
 
-    return updatedScoreArray;
-  }
+    return (
+      <div className="flex align-items-center gap-2">
+        <img
+          alt={representative.name}
+          src={`https://primefaces.org/cdn/primereact/images/avatar/${representative.image}`}
+          width="32"
+        />
+        <span>{representative.name}</span>
+      </div>
+    );
+  };
+
+  const representativesItemTemplate = (option) => {
+    return (
+      <div className="flex align-items-center gap-2">
+        <img
+          alt={option.name}
+          src={`https://primefaces.org/cdn/primereact/images/avatar/${option.image}`}
+          width="32"
+        />
+        <span>{option.name}</span>
+      </div>
+    );
+  };
+
+  const statusBodyTemplate = (rowData) => {
+    return (
+      <Tag value={rowData.status} severity={getSeverity(rowData.status)} />
+    );
+  };
+
+  const statusItemTemplate = (option) => {
+    return <Tag value={option} severity={getSeverity(option)} />;
+  };
+
+  const verifiedBodyTemplate = (rowData) => {
+    return (
+      <i
+        className={classNames("pi", {
+          "true-icon pi-check-circle": rowData.verified,
+          "false-icon pi-times-circle": !rowData.verified,
+        })}
+      ></i>
+    );
+  };
+
+  const representativeRowFilterTemplate = (options) => {
+    return (
+      <MultiSelect
+        value={options.value}
+        options={representatives}
+        itemTemplate={representativesItemTemplate}
+        onChange={(e) => options.filterApplyCallback(e.value)}
+        optionLabel="name"
+        placeholder="Any"
+        className="p-column-filter"
+        maxSelectedLabels={1}
+        style={{ minWidth: "14rem" }}
+      />
+    );
+  };
+
+  const statusRowFilterTemplate = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={statuses}
+        onChange={(e) => options.filterApplyCallback(e.value)}
+        itemTemplate={statusItemTemplate}
+        placeholder="Select One"
+        className="p-column-filter"
+        showClear
+        style={{ minWidth: "12rem" }}
+      />
+    );
+  };
+
+  const verifiedRowFilterTemplate = (options) => {
+    return (
+      <TriStateCheckbox
+        value={options.value}
+        onChange={(e) => options.filterApplyCallback(e.value)}
+      />
+    );
+  };
+
+  const header = renderHeader();
 
   return (
-    <section className="max-w-6xl mx-auto flex justify-center items-center flex-col mt-[100px]">
-      <h1 className="text-3xl text-center cursive">Leaderboard</h1>
-      <Leaderboard
-        className="max-w-6xl"
-        theme="cyan"
-        scoringMetric="Score"
-        id="rollno"
-        cell1="handle"
-        cell2="Score"
-        items={scoreArray}
-      ></Leaderboard>
-    </section>
+    <div className="card">
+      <DataTable
+        value={customers}
+        paginator
+        rows={10}
+        dataKey="id"
+        filters={filters}
+        filterDisplay="row"
+        loading={loading}
+        globalFilterFields={[
+          "name",
+          "country.name",
+          "representative.name",
+          "status",
+        ]}
+        header={header}
+        emptyMessage="No customers found."
+      >
+        <Column
+          field="name"
+          header="Name"
+          filter
+          filterPlaceholder="Search by name"
+          style={{ minWidth: "12rem" }}
+        />
+        <Column
+          header="Country"
+          filterField="country.name"
+          style={{ minWidth: "12rem" }}
+          body={countryBodyTemplate}
+          filter
+          filterPlaceholder="Search by country"
+        />
+        <Column
+          header="Agent"
+          filterField="representative"
+          showFilterMenu={false}
+          filterMenuStyle={{ width: "14rem" }}
+          style={{ minWidth: "14rem" }}
+          body={representativeBodyTemplate}
+          filter
+          filterElement={representativeRowFilterTemplate}
+        />
+        <Column
+          field="status"
+          header="Status"
+          showFilterMenu={false}
+          filterMenuStyle={{ width: "14rem" }}
+          style={{ minWidth: "12rem" }}
+          body={statusBodyTemplate}
+          filter
+          filterElement={statusRowFilterTemplate}
+        />
+        <Column
+          field="verified"
+          header="Verified"
+          dataType="boolean"
+          style={{ minWidth: "6rem" }}
+          body={verifiedBodyTemplate}
+          filter
+          filterElement={verifiedRowFilterTemplate}
+        />
+      </DataTable>
+    </div>
   );
 }
