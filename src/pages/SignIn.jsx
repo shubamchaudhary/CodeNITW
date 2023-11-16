7f8aa103e426beece34d411180633ab269ab16ff
SHUBAM CHAUDHARY <beshubam@gmail.com>
2023-11-16 17:20:13 +0530

    made sign in and sign up pages responsive

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,7 +5,6 @@ import { signOut , signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import computer from "../images/computer.png";
-
 export default function SignIn() {
   const [formData, setFormData] = useState({
     email: "",
@@ -51,7 +50,7 @@ export default function SignIn() {
     <div className="bg-blue-100 min-h-screen flex flex-col justify-center items-center">
       <section className="w-full max-w-8xl mx-auto flex flex-col md:flex-row justify-center items-center px-4 md:px-40">
         <div className="md:w-1/2 lg:w-1/2 mb-12 md:mb-6">
-          <img className="rounded-xl max-w-[700px]" src={computer} alt="" />
+          <img className="rounded-xl w-full md:max-w-[700px]" src={computer} alt="" />
         </div>
   
         <div className="w-full md:w-1/2 lg:w-1/2 lg:ml-20">
@@ -61,33 +60,33 @@ export default function SignIn() {
                 onChange={onChange}
                 id="email"
                 placeholder="Email address"
-                className="w-full h-[50px] p-4 text-2xl text-gray-700 bg-gray-100 rounded-lg mb-6"
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg mb-6"
                 type="email"
               ></input>
               <input
                 onChange={onChange}
                 type={showPassword ? "text" : "password"}
                 placeholder="Password"
-                className="w-full h-[50px] p-4 text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
                 id="password"
               ></input>
-              {showPassword ? (
+             {showPassword ? (
                 <AiFillEye
                   onClick={() => {
                     setShowPassword(!showPassword);
                   }}
-                  className="absolute text-3xl right-[10px] top-[85px]"
+                  className="absolute text-2xl md:text-3xl right-[5px] md:right-[10px] top-[45px] md:top-[85px]"
                 />
               ) : (
                 <AiFillEyeInvisible
                   onClick={() => {
                     setShowPassword(!showPassword);
                   }}
-                  className="absolute text-3xl right-[10px] top-[85px]"
+                  className="absolute text-2xl md:text-3xl right-[5px] md:right-[10px] top-[45px] md:top-[85px]"
                 />
               )}
             </div>
-            <div className="flex justify-between text-lg mt-2 mb-8">
+            <div className="flex justify-between text-sm md:text-lg mt-2 mb-8">
               <p>
                 Do Not have a Account?{" "}
                 <a className="text-red-600" href="/sign-up">
@@ -100,7 +99,7 @@ export default function SignIn() {
               </a>
             </div>
             <button
-              className="mb-4 mt-4 w-full text-lg bg-blue-900 text-white h-14 rounded-md  hover:bg-red-900 shadow-lg active:bg-red-950"
+              className="mb-4 mt-4 w-full text-lg bg-blue-900 text-white h-10 md:h-14 rounded-md  hover:bg-red-900 shadow-lg active:bg-red-950"
               type="submit"
             >
               Sign In
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -79,105 +79,103 @@ export default function SignUp() {
 
   return (
     <section>
-      {/* <h1 className="text-3xl text-center mt-6  cursive">Sign Up</h1> */}
-      <div className="flex flex-wrap justify-center items-center px-40 bg-blue-100 min-h-screen  max-w-8xl mx-auto">
-        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
-          <img className="rounded-xl max-w-[700px]" src={computer} alt="" />
+      <div className="flex flex-wrap justify-center items-center px-4 md:px-40 bg-blue-100 min-h-screen  max-w-8xl mx-auto">
+        <div className="w-full md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img className="rounded-xl w-full md:max-w-[700px]" src={computer} alt="" />
         </div>
-        <div className="w-[100%] lg:w-[40%] md:w-[67%] lg:ml-20">
+        <div className="w-full lg:w-[40%] md:w-[67%] lg:ml-20">
           <form onSubmit={onSubmit}>
             <div className="relative mb-6">
               <input
                 onChange={onChange}
                 id="name"
                 placeholder="Full Name"
-                className="w-full h-[50px] p-4 mb-4 text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 mb-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
                 type="text"
               ></input>
               <input
                 onChange={onChange}
                 id="email"
                 placeholder="Student Email"
-                className="w-full h-[50px] p-4 text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
                 type="email"
               ></input>
               <input
                 onChange={onChange}
                 id="cfhandle"
                 placeholder="Codeforces Handle"
-                className="w-full h-[50px] p-4 mt-4 mb-4 text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 mt-4 mb-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
                 type="text"
               ></input>
-              <div className="flex justify-center ">
+              <div className="flex flex-col md:flex-row justify-center ">
                 <input
                   onChange={onChange}
                   id="rollno"
                   placeholder="Roll Number"
-                  className="w-full h-[50px] p-4 mt-4 mb-4 mr-2 text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                  className="w-full h-10 md:h-[50px] p-2 md:p-4 mt-4 mb-4 mr-2 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
                   type="text"
                 ></input>
                 <select
                   id="course"
                   onChange={onChange}
-                  className="w-full h-[50px] p-4 mt-4 mb-4 mx-2 text-gray-700 bg-gray-100 rounded-lg"
-                >
-                  <option id="btech" value="btech">
-                    BTech
-                  </option>
-                  <option id="mtech" value="mtech">
-                    MTech
-                  </option>
-                  <option id="msc" value="msc">
-                    MSC
-                  </option>
-                  <option id="mac" value="mac">
-                    MCA
-                  </option>
-                </select>
-                <select
-                  id="year"
-                  className="w-full h-[50px] p-4 mt-4 ml-2 mb-4 text-gray-700 bg-gray-100 rounded-lg"
-                  onChange={onChange}
-                >
-                  <option id="first" value="first">
-                    1st Year
-                  </option>
-                  <option id="second" value="second">
-                    2nd Year
-                  </option>
-                  <option id="third" value="third">
-                    3rd Year
-                  </option>
-                  <option id="fourth" value="fourth">
-                    4th Year
-                  </option>
-                </select>
-              </div>
-              <input
+                  className="w-full h-10 md:h-[50px] p-2 md:p-4 mt-4 mb-4 mx-2 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                  >
+                    <option id="btech" value="btech">
+                      BTech
+                    </option>
+                    <option id="mtech" value="mtech">
+                      MTech
+                    </option>
+                    <option id="msc" value="msc">
+                      MSC
+                    </option>
+                    <option id="mac" value="mac">
+                      MCA
+                    </option>
+                  </select>
+                  <select
+                    id="year"
+                    className="w-full h-10 md:h-[50px] p-2 md:p-4 mt-4 ml-2 mb-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg"
+                    onChange={onChange}
+                  >
+                    <option id="first" value="first">
+                      1st Year
+                    </option>
+                    <option id="second" value="second">
+                      2nd Year
+                    </option>
+                    <option id="third" value="third">
+                      3rd Year
+                    </option>
+                    <option id="fourth" value="fourth">
+                      4th Year
+                    </option>
+                  </select>
+                </div>
+                <input
                 onChange={onChange}
                 type={showPassword ? "text" : "password"}
                 placeholder="Password"
-                className="w-full h-[50px] p-4 mt-4 text-2xl text-gray-700 bg-gray-100 rounded-lg "
+                className="w-full h-10 md:h-[50px] p-2 md:p-4 mt-4 text-lg md:text-2xl text-gray-700 bg-gray-100 rounded-lg "
                 id="password"
               ></input>
-              {/* <button className='absolute text-xl right-[10px] top-[58px]' onClick={()=>{setShowPassword(!showPassword)}}>{showPassword ? <AiFillEye /> : <AiFillEyeInvisible /> }</button> */}
               {showPassword ? (
                 <AiFillEye
                   onClick={() => {
                     setShowPassword(!showPassword);
                   }}
-                  className="absolute text-3xl right-[10px] top-[308px]"
+                  className="absolute text-2xl md:text-3xl right-[5px] md:right-[10px] top-[308px] md:top-[308px]"
                 />
               ) : (
                 <AiFillEyeInvisible
                   onClick={() => {
                     setShowPassword(!showPassword);
                   }}
-                  className="absolute text-3xl right-[10px] top-[308px]"
+                  className="absolute text-2xl md:text-3xl right-[5px] md:right-[10px] top-[308px] md:top-[308px]"
                 />
               )}
             </div>
-            <div className="flex justify-between text-lg mt-2 mb-8">
+            <div className="flex justify-between text-sm md:text-lg mt-2 mb-8">
               <p>
                 Have an Account?{" "}
                 <a className="text-red-600" href="/sign-in">
@@ -189,15 +187,11 @@ export default function SignUp() {
               </a>
             </div>
             <button
-              className="mb-4 mt-4 w-full text-lg bg-blue-900 text-white h-14 rounded-md  hover:bg-red-900 shadow-lg active:bg-red-950"
+              className="mb-4 mt-4 w-full text-lg bg-blue-900 text-white h-10 md:h-14 rounded-md  hover:bg-red-900 shadow-lg active:bg-red-950"
               type="submit"
             >
               Sign Up
             </button>
-            {/* <div className="mt-2 mb-2 ">
-              <p className="text-center">OR</p>
-            </div>
-            <OAuth></OAuth> */}
           </form>
         </div>
       </div>
