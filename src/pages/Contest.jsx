5647d266ff6269b836a778d63929c8f93cb2995b
SHUBAM CHAUDHARY <beshubam@gmail.com>
2023-12-14 10:34:34 +0530

    other contests removed

--- src/pages/Contest.jsx
+++ src/pages/Contest.jsx
@@ -119,7 +119,7 @@ export default function Contest() {
     }
     
     fetchListings();
-    getOtherContest();
+    //getOtherContest();
   }, []);
 
   
@@ -170,7 +170,7 @@ export default function Contest() {
   )}
 </div>
 </div>
- <div className="max-w-6xl mx-auto pt-4 space-y-6">
+ {/* <div className="max-w-6xl mx-auto pt-4 space-y-6">
 <div className="m-2 mb-6 bg-white dark:bg-[#2C2C2EFF] shadow-lg p-4 rounded-lg">
   <h1 className="text-4xl font-semibold text-blue-600 dark:text-blue-500 mb-4">Other Contests</h1>
 
@@ -201,7 +201,7 @@ export default function Contest() {
     </div>
   )}
 </div>
-</div> 
+</div>  */}
 </div>
 
   );
