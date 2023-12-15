cc9e9ae75385929866414e68a08ade3f5de4099a
SHUBAM CHAUDHARY <beshubam@gmail.com>
2023-12-15 23:16:59 +0530

    ot material added

--- src/pages/OTMaterial.jsx
+++ src/pages/OTMaterial.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OTMaterial() {
   return (
     <div>
-      
+      <img src='https://1specialplace.com/wp-content/uploads/2022/02/ball.jpg' className='w-screen'></img>
     </div>
   )
 }
